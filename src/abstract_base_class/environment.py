73ad2ba7a6e998ffd3760c6b553c5938332a24d9
Amrita <amrita.raychaudhury@rwth-aachen.de>
2022-11-15 00:20:19 +0100

    abstract class commented code removed

--- src/abstract_base_class/environment.py
+++ src/abstract_base_class/environment.py
@@ -1,12 +1,17 @@
 from abc import ABC, abstractmethod
 import numpy as np
-from abstract_base_class.experiment_tracking import AbstractExperimentTracking
-from abstract_base_class.reward import AbstractReward
-from abstract_base_class.interfaces import ModelInterface
+import gym
+
+from gym import error, logger, spaces
+from gym.spaces import Space
+
+from experiment_tracking import AbstractExperimentTracking
+from reward import AbstractReward
+from interfaces import ModelInterface
 
 
 
-class AbstractITAEnvironment(ABC):
+class AbstractITAEnvironment(ABC, gym.Env):
 
     @property
     @abstractmethod
@@ -35,21 +40,26 @@ class AbstractITAEnvironment(ABC):
 
     @property
     @abstractmethod
-    def current_state(self) -> current_state:
+    def current_state(self) -> bool:
         pass
 
-    @abstractmethod
-    def define_state_space(self):
-        pass
+    # @abstractmethod
+    # def define_state_space(self):
+    #     pass
 
-    @abstractmethod
-    def step(self, action:) -> observation, reward, termination, truncation, info :
-        pass
+    # @abstractmethod
+    # def step(self, action) -> (observation:int, reward:int, termination:int, truncation:int, info:int) :
+    #     pass
 
-    @abstractmethod
-    def define_action_space(self):
-        pass
+    # @abstractmethod
+    # def define_action_space(self):
+    #     pass
 
-    @abstractmethod
-    def reset(self):
-        pass
+    # @abstractmethod
+    # def reset(self):
+    #     pass
+
+
+    # ai gym wrapper , or inherit, implement class keeping type same but with dummy values (machine model - arrays of 1 for ex.)
+    # implement functions of env not other classes
+    # check ai gym env implementation
--- src/abstract_base_class/reward.py
+++ src/abstract_base_class/reward.py
@@ -14,16 +14,6 @@ class AbstractReward(ABC):
     def reward_value(self) -> float :
         pass
 
-    # @ITA_requirements.setter
-    # @abstractmethod
-    # def ITA_requirements(self, requirements:dict) :
-    #     pass
-
-    @reward_value.setter
-    @abstractmethod
-    def reward_value(self, reward_value:float):
-        pass
-
     @abstractmethod
     def calculate_reward(self, current_state:np.array, current_model_output:np.array, safety_flag:bool) -> float :
         pass
@@ -31,46 +21,3 @@ class AbstractReward(ABC):
     @abstractmethod
     def penalty(self) -> float :
         pass
-
-
-class Reward(AbstractReward):
-
-
-    def __init__(self, ITA_requirements, reward_value=0.0):
-        self._ITA_requirements = ITA_requirements
-        self._reward_value = reward_value
-
-    @property
-    def ITA_requirements(self) -> dict :
-        return self._ITA_requirements
-
-    @property
-    def reward_value(self) -> float :
-        return self._reward_value
-
-    @ITA_requirements.setter
-    def ITA_requirements(self, requirements:dict) :
-        self._ITA_requirements = requirements
-
-    @reward_value.setter
-    def reward_value(self, reward_value:float):
-        self._reward_value = reward_value
-
-    def calculate_reward(self, current_state: np.array, current_model_output: np.array, safety_flag: bool) -> float:
-        print("Calculating reward")
-        return 5.0
-
-    def penalty(self) -> float:
-        print("penaltyyyy")
-        return 2.0
-
-def main():
-    requirements = {'a': 1, 'b': 2}
-    reward = Reward(requirements)
-    reward.reward_value=5
-    print(reward.ITA_requirements)
-    print(reward.penalty())
-    print(reward.reward_value)
-    print(reward._reward_value)
-
-main()

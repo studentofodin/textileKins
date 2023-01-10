74821a32ef4714d1a6bf4ef9f14a4d5853150b55
unknown <luis.kutschat@rwth-aachen.de>
2023-01-10 22:00:28 +0100

    deleted mapActionsToInputs, added load_model

--- src/base_classes/model_wrapper.py
+++ src/base_classes/model_wrapper.py
@@ -1,23 +1,38 @@
-import numpy as np
-from src.abstract_base_class.model_wrapper import AbstractModelWrapper
-from src.base_classes.model_interface import ModelInterface
+import pathlib as pl
+import dill
+import pickle
 
+from src.abstract_base_class.model_wrapper import AbstractModelWrapper
+from src.base_classes.model_interface import *
 
 class ModelWrapper(AbstractModelWrapper):
 
+    @staticmethod
+    def load_model(model_props: dict) -> AbstractModelInterface:
+        with open(pl.Path(model_props["model_path"]), "rb") as file:
+            pickle_obj = pickle.load(file)
+        model_class = model_props["model_class"]
+        if model_class == "SVGP":
+            mdl = AdapterSVGP(pickle_obj, model_props, rescale_y=True)
+        elif model_class == "GPy_GPR":
+            mdl = AdapterGPy(pickle_obj, model_props, rescale_y=True)
+        else:
+            raise (TypeError(f"The model class {model_class} is not yet supported"))
+        return mdl
+
     @property
-    def machineModel(self) -> ModelInterface:
+    def model(self) -> AbstractModelInterface:
         return self._machineModel
 
     @machineModel.setter
     def machineModel(self, machineModel):
         self._machineModel = machineModel
 
-    def __init__(self):
-        self.machineModel = ModelInterface({
-                                                "inputs": ["input_d", "input_c"],
-                                                "output": "target_b"
-                                            })
+    # def __init__(self):
+    #     self.machineModel ModelInterface({
+    #                                             "inputs": ["input_d", "input_c"],
+    #                                             "output": "target_b"
+    #                                         })
 
     def mapActionsToInputs(self, action):
         return np.array([3,2,1])
@@ -28,9 +43,7 @@ class ModelWrapper(AbstractModelWrapper):
     def interpretModelOutputs(self, mean: np.array, lowerConfidence: np.array, upperConfidence: np.array) -> np.array:
         return [1,2,3]
 
-
     def getOutput(self, action):
-        input = self.mapActionsToInputs(action)
         mean, lowerConfidence, upperConfidence = self.callMachineModel(input)
         output = self.interpretModelOutputs(mean, lowerConfidence, upperConfidence)
         return output

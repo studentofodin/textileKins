f2d86b7b00240a59ab3b584dd34803b49b2a540a
unknown <luis.kutschat@rwth-aachen.de>
2023-01-10 20:37:02 +0100

    made everthing snake_case, property is a list of models instead of one model, deleted mapActionsToInputs(), added internal methods, adapted methods to Ruben's models

--- src/abstract_base_class/model_wrapper.py
+++ src/abstract_base_class/model_wrapper.py
@@ -1,29 +1,41 @@
 from abc import ABC, abstractmethod
-
 import numpy as np
+from typing import Tuple
+from typing import List
 
-from src.base_classes.model_interface import ModelInterface
+from src.abstract_base_class.model_interface import AbstractModelInterface
+from src.abstract_base_class.safety_wrapper import AbstractSafetyWrapper
+
 
 
 class AbstractModelWrapper(ABC):
 
+    @staticmethod
+    @abstractmethod
+    def load_model(model_props: dict) -> AbstractModelInterface:
+        pass
+
     @property
     @abstractmethod
-    def machineModel(self) -> ModelInterface:
+    def machine_models(self) -> List[AbstractModelInterface, ...]:
         pass
 
     @abstractmethod
-    def mapActionsToInputs(self, action: np.array) -> np.array:
+    def interpret_model_outputs(self, mean: np.array, var: np.array) -> np.array:
         pass
 
     @abstractmethod
-    def interpretModelOutputs(self, mean: np.array, lowerConfidence: np.array, upperConfidence: np.array) -> np.array:
+    def call_models(self, input: dict, latent: bool) -> Tuple[np.array, np.array]:
         pass
 
     @abstractmethod
-    def callMachineModel(self, input: np.array) -> list[np.array, np.array, np.array]:
+    def call_models_internal(self, input: np.array, latent: bool) -> Tuple[np.array, np.array]:
         pass
 
     @abstractmethod
-    def getOutput(self, action: np.array) -> np.array:
+    def get_output(self, input: dict, latent: bool) -> np.array:
+        pass
+
+    @abstractmethod
+    def get_output_internal(self, input: np.array, latent: bool) -> np.array:
         pass

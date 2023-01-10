8ad933c41d7e39b135b62823067fbd2d2ea30780
unknown <luis.kutschat@rwth-aachen.de>
2023-01-10 20:04:28 +0100

    the predict methods return tuples instead of lists

--- src/base_classes/model_interface.py
+++ src/base_classes/model_interface.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.pipeline import Pipeline
 from copy import deepcopy
+from typing import Tuple
 
 from src.abstract_base_class.model_interface import AbstractModelInterface
 
@@ -35,7 +36,7 @@ class AdapterSVGP(AbstractModelInterface):
     def model_properties(self) -> dict:
         return deepcopy(self._model_properties)
 
-    def predict_f_internal(self, X: np.array) -> [np.array, np.array]:
+    def predict_f_internal(self, X: np.array) -> Tuple[np.array, np.array]:
         X_trans = self._pipe.transform(X)
         y_pred, var = self._model.predict_f(X_trans)
         y_pred, var = y_pred.numpy(), var.numpy()
@@ -44,7 +45,7 @@ class AdapterSVGP(AbstractModelInterface):
             var = var * self._scaler_y.scale_[0]
         return y_pred.numpy(), var.numpy()
 
-    def predict_y_internal(self, X: np.array) -> [np.array, np.array]:
+    def predict_y_internal(self, X: np.array) -> Tuple[np.array, np.array]:
         X_trans = self._pipe.transform(X)
         y_pred, var = self._model.predict_y(X_trans)
         y_pred, var = y_pred.numpy(), var.numpy()
@@ -53,12 +54,12 @@ class AdapterSVGP(AbstractModelInterface):
             var = var * self._scaler_y.scale_[0]
         return y_pred.numpy(), var.numpy()
 
-    def predict_f(self, X: dict) -> [np.array, np.array]:
+    def predict_f(self, X: dict) -> Tuple[np.array, np.array]:
         X = self._unpack_func(X)
         y_pred, var = self.predict_f_internal(X)
         return y_pred, var
 
-    def predict_y(self, X: dict) -> [np.array, np.array]:
+    def predict_y(self, X: dict) -> Tuple[np.array, np.array]:
         X = self._unpack_func(X)
         y_pred, var = self.predict_y_internal(X)
         return y_pred, var
@@ -93,7 +94,7 @@ class AdapterGPy(AbstractModelInterface):
     def model_properties(self) -> dict:
         return deepcopy(self._model_properties)
 
-    def predict_f_internal(self, X: np.array) -> [np.array, np.array]:
+    def predict_f_internal(self, X: np.array) -> Tuple[np.array, np.array]:
         X_trans = self._pipe.transform(X)
         y_pred, var = self._model.predict_noiseless(X_trans)
         if self._scaler_y is not None and self._rescale_y:
@@ -101,7 +102,7 @@ class AdapterGPy(AbstractModelInterface):
             var = var * self._scaler_y.scale_[0]
         return y_pred, var
 
-    def predict_y_internal(self, X: np.array) -> [np.array, np.array]:
+    def predict_y_internal(self, X: np.array) -> Tuple[np.array, np.array]:
         X_trans = self._pipe.transform(X)
         y_pred, var = self._model.predict(X_trans)
         if self._scaler_y is not None and self._rescale_y:
@@ -109,12 +110,12 @@ class AdapterGPy(AbstractModelInterface):
             var = var * self._scaler_y.scale_[0]
         return y_pred, var
 
-    def predict_f(self, X: dict) -> np.array:
+    def predict_f(self, X: dict) -> Tuple[np.array, np.array]:
         X = self._unpack_func(X)
         y_pred, var = self.predict_f_internal(X)
         return y_pred, var
 
-    def predict_y(self, X: dict) -> np.array:
+    def predict_y(self, X: dict) -> Tuple[np.array, np.array]:
         X = self._unpack_func(X)
         y_pred, var = self.predict_y_internal(X)
         return y_pred, var

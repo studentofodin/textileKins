3e9a933a9c363174a702cf49b9f86ab6bab004d1
unknown <luis.kutschat@rwth-aachen.de>
2023-01-23 19:20:29 +0100

    changed inputs dict

--- test_models.py
+++ test_models.py
@@ -19,12 +19,15 @@ for i in range(len(model_names)):
 
 model_wrapper = ModelWrapper(model_props, model_dirs)
 
-keys = ["Ishikawa_WeightPerAreaCardDelivery", "Ishikawa_CardMassThroughputSetpoint", "Ishikawa_LayersCount",
-        "Ishikawa_DraftRatioNeedleloom1Intake", "Ishikawa_DraftRatioNeedleloom", "v_Vorreisser", "v_Arbeiter_HT",
-        "v_Wender_HT", "v_Arbeiter_VR", "v_Wender_VR"]
-values = np.ones((len(keys)))
+# keys = ["Ishikawa_WeightPerAreaCardDelivery", "Ishikawa_CardMassThroughputSetpoint", "Ishikawa_LayersCount",
+#         "Ishikawa_DraftRatioNeedleloom1Intake", "Ishikawa_DraftRatioNeedleloom", "v_Vorreisser", "v_Arbeiter_HT",
+#         "v_Wender_HT", "v_Arbeiter_VR", "v_Wender_VR"]
+# values = np.ones((len(keys)))
+#
+# inputs = dict(zip(keys, values))
 
-inputs = dict(zip(keys, values))
+inputs = {"mean_cylinder_worker":1, "Ishikawa_WeightPerAreaCardDelivery":5, "v_Abnehmer":4, "v_Arbeiter_HT":2, "v_Wender_HT":8,
+          "Ishikawa_CardMassThroughputSetpoint":1, "v_Arbeiter_HT":6, "v_Arbeiter_VR":1, "v_Wender_VR":5}
 
 print(model_wrapper.get_outputs(inputs))
 

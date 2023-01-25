806d97830ea97b6993edef6e9e99e6a9be0547ef
Amrita <amrita.raychaudhury@rwth-aachen.de>
2023-01-25 23:45:45 +0100

    gym wrapper- removed close function

--- src/base_classes/env.py
+++ src/base_classes/env.py
@@ -114,19 +114,13 @@ class TrainingEnvironment(AbstractTrainingEnvironment):
             observation (object): this will be an element of the environment's :attr:`observation_space`.
                 This may, for instance, be a numpy array containing the positions and velocities of certain objects.
             reward (float): The amount of reward returned as a result of taking the action.
-            terminated (bool): whether a `terminal state` (as defined under the MDP of the task) is reached.
-                In this case further step() calls could return undefined results.
-            truncated (bool): whether a truncation condition outside the scope of the MDP is satisfied.
-                Typically a timelimit, but could also be used to indicate agent physically going out of bounds.
-                Can be used to end the episode prematurely before a `terminal state` is reached.
+            terminated (bool)/truncated (bool) : not needed as safety limits checked by safety wrapper
             info (dictionary): `info` contains auxiliary diagnostic information (helpful for debugging, learning, and logging).
                 This might, for instance, contain: metrics that describe the agent's performance state, variables that are
                 hidden from observations, or individual reward terms that are combined to produce the total reward.
                 It also can contain information that distinguishes truncation and termination, however this is deprecated in favour
                 of returning two booleans, and will be removed in a future version.
-            done (bool): A boolean value for if the episode has ended, in which case further :meth:`step` calls will return undefined results.
-                A done signal may be emitted for different reasons: Maybe the task underlying the environment was solved successfully,
-                a certain timelimit was exceeded, or the physics simulation has entered an invalid state.
+            done (bool): Not needed since it is a continuous process
         """
         safetyFlag = self.calculateStateFromAction(action)
         observationArray, observationDict = self.machine.getOutput(self.currentState)
@@ -139,9 +133,13 @@ class TrainingEnvironment(AbstractTrainingEnvironment):
     def reset(self):
         # Reset Current State to Initial State
         # Reset required variables to start optimisation again
+        # super().reset()
         self._currentState = dict(self.config.initialState)
         self._reward = 0.0
         self.done = False
+        observationArray, _ = self.machine.getOutput(self.currentState)
+        info = {}
+        return observationArray, info
 
     def calculateStateFromAction(self, action):
         updatedState = {}

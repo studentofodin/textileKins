07b9efa439a117b05548ceaafb901c4e45857f72
Amrita <amrita.raychaudhury@rwth-aachen.de>
2023-01-25 21:09:26 +0100

    testing env step function

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def main(configuration : DictConfig):
     print(scenarioManager.disturbanceSetting)
     scenarioManager.disturbanceSetting = {'key5': 5, 'key6': 6}
     print(scenarioManager.disturbanceSetting)
-    experimentTracker = ExperimentTracker(config.experimentTracker.metrics)
+    experimentTracker = ExperimentTracker(config)
     print("Experiment Tracker", experimentTracker.metrics)
     reward = Reward(config.reward)
     print("Reward", reward._config)
@@ -27,7 +27,10 @@ def main(configuration : DictConfig):
     trainingEnv = TrainingEnvironment(config.env, ModelWrapper(), reward, experimentTracker, safetyWrapper)
     env = GymWrapper(env=trainingEnv)
 
-    env.step(np.array([5.0,3.0,4.0]))
+    for _ in range(45):
+        print(env.step(np.random.randn(3)))
+
+
 
 if __name__ == "__main__":
     main()

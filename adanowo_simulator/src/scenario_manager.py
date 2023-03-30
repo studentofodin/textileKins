4660144eef7879e0c86a2d6b0029cb1169ad7583
luis.kutschat <luis.kutschat@rwth-aachen.de>
2023-03-30 17:57:37 +0200

    minor changes

--- adanowo_simulator/src/scenario_manager.py
+++ adanowo_simulator/src/scenario_manager.py
@@ -1,32 +1,24 @@
-import pathlib as pl
-import yaml
 from omegaconf import DictConfig
-import numpy as np
 from typing import List
 
 from src.abstract_base_class.scenario_manager import AbstractScenarioManager
 
-
 class ScenarioManager(AbstractScenarioManager):
 
     def __init__(self, config: DictConfig, model_wrapper_config: DictConfig):
-        # model_wrapper_config.outputModels["unevenness_card_web"] = "unevenness_card_web1"
         self._config = config
 
         # check if output model scenarios are valid.
-        for output_name, scenario in self._config.outputModels.items():
-            self._check_simple_scenario(scenario, "outputModels", output_name)
+        for output, scenario in self._config.outputModels.items():
+            self._check_simple_scenario(scenario, "outputModels", output)
 
         # check if requirement scenarios are valid.
-        for output_name, scenario in self._config.requirements.simpleOutputBounds.lower.items():
-            self._check_simple_scenario(scenario, "requirements/simpleOutputBounds/lower", output_name)
-        for output_name, scenario in self._config.requirements.simpleOutputBounds.upper.items():
-            self._check_simple_scenario(scenario, "requirements/simpleOutputBounds/upper", output_name)
-        for name, scenario in self._config.requirements.complexConstraints.items():
-            self._check_simple_scenario(scenario, "requirements/simpleOutputBounds/upper", output_name)
-
-
-
+        for output, scenario in self._config.requirements.simpleOutputBounds.lower.items():
+            self._check_simple_scenario(scenario, "requirements/simpleOutputBounds/lower", output)
+        for output, scenario in self._config.requirements.simpleOutputBounds.upper.items():
+            self._check_simple_scenario(scenario, "requirements/simpleOutputBounds/upper", output)
+        for req, scenario in self._config.requirements.complexConstraints.items():
+            self._check_simple_scenario(scenario, "requirements/simpleOutputBounds/upper", req)
 
     @property
     def config(self) -> DictConfig:
@@ -40,30 +32,30 @@ class ScenarioManager(AbstractScenarioManager):
 
         changed = []
 
-        for output_name, scenario in self._config.outputModels.items():
+        for output, scenario in self._config.outputModels.items():
             if scenario and scenario[0][0] == step_index:
-                output_models_config[output_name] = scenario[0][1]
-                changed.append(output_name)
-                self._config.outputModels[output_name].pop(0)
+                output_models_config[output] = scenario[0][1]
+                changed.append(output)
+                self._config.outputModels[output].pop(0)
 
         return changed
 
     def update_requirements(self, step_index: int, requirements_config: DictConfig) -> None:
 
-        for output_name, scenario in self._config.requirements.simpleOutputBounds.lower.items():
+        for output, scenario in self._config.requirements.simpleOutputBounds.lower.items():
             if scenario and scenario[0][0] == step_index:
-                requirements_config.simpleOutputBounds.lower.output_name = scenario[0][1]
-                self._config.requirements.simpleOutputBounds.lower[output_name].pop(0)
+                requirements_config.simpleOutputBounds.lower[output] = scenario[0][1]
+                self._config.requirements.simpleOutputBounds.lower[output].pop(0)
 
-        for output_name, scenario in self._config.requirements.simpleOutputBounds.upper.items():
+        for output, scenario in self._config.requirements.simpleOutputBounds.upper.items():
             if scenario and scenario[0][0] == step_index:
-                requirements_config.simpleOutputBounds.upper[output_name] = scenario[0][1]
-                self._config.requirements.simpleOutputBounds.upper[output_name].pop(0)
+                requirements_config.simpleOutputBounds.upper[output] = scenario[0][1]
+                self._config.requirements.simpleOutputBounds.upper[output].pop(0)
 
-        for name, scenario in self._config.requirements.complexConstraints.items():
+        for req, scenario in self._config.requirements.complexConstraints.items():
             if scenario and scenario[0][0] == step_index:
-                requirements_config.complexConstraints[name] = scenario[0][1]
-                self._config.requirements.complexConstraints[name].pop(0)
+                requirements_config.complexConstraints[req] = scenario[0][1]
+                self._config.requirements.complexConstraints[req].pop(0)
 
 
 

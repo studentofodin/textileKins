bcdd6d6be3c9b4ba7183fe893b7d169ad3286ae4
luis.kutschat <luis.kutschat@rwth-aachen.de>
2023-05-30 11:28:22 +0200

    adapt reward_functions to new lineSpeed model and make it actually return float instead of np.array

--- adanowo_simulator/src/reward_functions.py
+++ adanowo_simulator/src/reward_functions.py
@@ -1,17 +1,14 @@
-from src.helper_functions import calc_line_speed
-
 MIN_TO_H = 60
 
 
 def baseline_reward(state: dict[str, float], outputs: dict[str, float], config) -> float:
-    line_speed = calc_line_speed(state)
 
     # material costs
     material_costs = state["CardMassThroughputSetpoint"] * config.fibre_costs
     # energy costs
     energy_costs = outputs["linePowerConsumption"] * config.energy_costs
     # Production income
-    income = config.selling_price * line_speed * MIN_TO_H * state["ProductWidth"]
+    income = config.selling_price * outputs["lineSpeed"] * MIN_TO_H * state["ProductWidth"]
 
     # Component 1: Calculate economic efficiency
     contribution_margin = income - energy_costs - material_costs
@@ -21,4 +18,4 @@ def baseline_reward(state: dict[str, float], outputs: dict[str, float], config) 
 
     reward = contribution_margin - floor_quality
 
-    return reward
+    return reward.item()

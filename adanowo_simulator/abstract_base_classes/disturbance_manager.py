331397eabbd726d63cc0c3e28bea1b44a965dad1
unknown <luis.kutschat@rwth-aachen.de>
2023-09-25 17:52:31 +0200

    Add docstrins for AbstractDisturbanceManager.

--- adanowo_simulator/abstract_base_classes/disturbance_manager.py
+++ adanowo_simulator/abstract_base_classes/disturbance_manager.py
@@ -3,9 +3,15 @@ from omegaconf import DictConfig
 
 
 class AbstractDisturbanceManager(ABC):
+    """Abstract class for a disturbance manager.
+
+    A disturbance manager gets disturbances.
+    It is not useful on its own and should be a member of an
+    :py:class:'~adanowo_simulator.abstract_base_classes.environment.AbstractEnvironment'."""
     @property
     @abstractmethod
     def config(self) -> DictConfig:
+        """Configuration of a disturbance manager."""
         pass
 
     @config.setter
@@ -15,12 +21,15 @@ class AbstractDisturbanceManager(ABC):
 
     @abstractmethod
     def step(self) -> dict[str, float]:
+        """Gets disturbances and returns them."""
         pass
 
     @abstractmethod
     def reset(self) -> dict[str, float]:
+        """Resets the disturbance manager to initial values and returns the initial disturbances."""
         pass
 
     @abstractmethod
     def close(self) -> None:
+        """Closes the disturbance manager."""
         pass

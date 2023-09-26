8c831c6dac3815cd48abc5b83201cb4e11efb460
unknown <luis.kutschat@rwth-aachen.de>
2023-09-26 18:40:16 +0200

    Added docstrings for AbstractCalculationAdapter

--- adanowo_simulator/abstract_base_classes/calculation_adapter.py
+++ adanowo_simulator/abstract_base_classes/calculation_adapter.py
@@ -3,7 +3,14 @@ from abc import ABC, abstractmethod
 
 
 class AbstractCalculationAdapter(ABC):
+    """Abstract class for a calculation adapter.
+
+    A calculation adapter performs calculations. Assume variable y depends on a variable x.
+    * We use the term calculation if, given x, a value for y is directly returned.
+    * We use the term model if, given x, a probability distribution for y is returned.
+    To get a value you have to sample from the modeled distribution."""
 
     @abstractmethod
     def calculate(self, X: dict[str, float]) -> np.array:
+        """Performs a calculation."""
         pass
